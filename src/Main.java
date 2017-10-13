f823dc3d100ad25bfdeb93179550c45f0c3f58ef
Joanna Niklus <joanna.niklus@gmail.com>
2017-10-13 14:05:05 +0300

    test

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello, world.\nThis is a game of five in a row.");
+		// testing things
 	}
 
 }
