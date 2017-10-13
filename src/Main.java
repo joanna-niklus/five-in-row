d4d2f057a54659e7762569e89b4f14561f7c89b2
Joanna Niklus <joanna.niklus@hotmail.com>
2017-10-13 13:54:43 +0300

    New project

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, world.\nThis is a game of five in a row.");
+	}
+
+}
